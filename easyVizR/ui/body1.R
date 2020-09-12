edb19a747b8c0f4878b7d09243c132048c4b0ace
Judith Y <47602796+judithayan@users.noreply.github.com>
2020-09-11 20:15:12 -0700

    easyvizR changed to upload modals; lots of bug fixes

--- easyVizR/ui/body1.R
+++ easyVizR/ui/body1.R
@@ -21,7 +21,8 @@ body1 <- tabItem(tabName = "tab1",
                                                   title = span( icon("upload"), "Single File Upload"),  status = "primary", solidHeader = F,
                                                   id = "tab1",
                                                   
-                                                  fileInput("file", "Upload a single CSV file:",
+                                                  fileInput("file", 
+                                                            label_with_help_bttn("Upload a single CSV file:", "upload_q"),
                                                             accept = c(
                                                               "text/csv",
                                                               "text/comma-separated-values,text/plain",
@@ -30,16 +31,17 @@ body1 <- tabItem(tabName = "tab1",
                                                   
                                                   
                                                   
-                                                  uiOutput("uploaded_file"),
-                                                  uiOutput("uploaded_file_name"),
                                                   
-                                                  # only show these buttons when file is uploaded.
-                                                  uiOutput("cn_ins"),
-                                                  uiOutput("cn_1"),
-                                                  uiOutput("cn_2"),
-                                                  uiOutput("load_other_cols"),
-                                                  uiOutput("cn_feedback"),
-                                                  uiOutput("cn_3"),
+                                                  
+                                                  # uiOutput("uploaded_file"),
+                                                  # uiOutput("uploaded_file_name"),
+                                                  # # # only show these buttons when file is uploaded.
+                                                  # uiOutput("cn_ins"),
+                                                  # uiOutput("cn_1"),
+                                                  # uiOutput("cn_2"),
+                                                  # uiOutput("load_other_cols"),
+                                                  # uiOutput("cn_feedback"),
+                                                  # uiOutput("cn_3"),
                                               ),
                                               
                               ))
@@ -50,7 +52,8 @@ body1 <- tabItem(tabName = "tab1",
                                                   title = span( icon("folder-open"), "Batch upload"),  status = "primary", solidHeader = F,
                                                   
                                                   tags$div(class="form-group shiny-input-container", 
-                                                           tags$div(tags$label("Upload a folder containing CSV files:")),
+                                                           tags$div(label_with_help_bttn(tags$label("Upload a folder containing CSV files:"), "upload_batch_q")
+                                                             ),
                                                            tags$div(tags$label("Select folder", class="btn btn-primary",
                                                                                tags$input(id = "fileIn", webkitdirectory = TRUE, type = "file", style="display: none;", onchange="pressed()"))),
                                                            tags$label("", id = "noFile"), # need to fix
@@ -58,34 +61,34 @@ body1 <- tabItem(tabName = "tab1",
                                                                     tags$div(class="progress-bar") # somehow this breaks the upload = =..
                                                            )     
                                                   ),
-                                                  uiOutput("batch_feedback_1"),
-                                                  uiOutput("batch_opt_1"),
-                                                  uiOutput("batch_opt_2"),
-                                                  uiOutput("batch_additional_cols"),
-                                                  uiOutput("batch_feedback_2"),
-                                                  uiOutput("batch_feedback_3"),
+                                                  # uiOutput("batch_feedback_1"),
+                                                  # uiOutput("batch_opt_1"),
+                                                  # uiOutput("batch_opt_2"),
+                                                  # uiOutput("batch_additional_cols"),
+                                                  # uiOutput("batch_feedback_2"),
+                                                  # uiOutput("batch_feedback_3"),
                                                   
-                                                  uiOutput("ug_3")
+                                                  # uiOutput("ug_3")
                                               ),
                               )),
                           ),
                           
-                          div(id="u_1c",
-                              fluidRow(column(12,
-                                              box(width=12,
-                                                  title = span( icon("table"), "From existing dataset"), status = "primary", solidHeader = F,
-                                                  collapsible = T,
-                                                  tabPanel("Loaded files", 
-                                                           uiOutput("add_subset_select"),
-                                                           uiOutput("add_subset_opt"),
-                                                           uiOutput("add_subset_preview"),
-                                                           uiOutput("add_subset_name"),
-                                                           
-                                                           uiOutput("add_subset_confirm"),
-                                                  )
-                                              )
-                              ))
-                          ),
+                          # div(id="u_1c",
+                          #     fluidRow(column(12,
+                          #                     box(width=12,
+                          #                         title = span( icon("table"), "From existing dataset"), status = "primary", solidHeader = F,
+                          #                         collapsible = T,
+                          #                         tabPanel("Loaded files", 
+                          #                                  uiOutput("add_subset_select"),
+                          #                                  uiOutput("add_subset_opt"),
+                          #                                  uiOutput("add_subset_preview"),
+                          #                                  uiOutput("add_subset_name"),
+                          #                                  
+                          #                                  uiOutput("add_subset_confirm"),
+                          #                         )
+                          #                     )
+                          #     ))
+                          # ),
                           
                    ),
                    
