814440e6b94f8cbda2a613a7f6fd1b1b6063c256
easygsea <easygsea@gmail.com>
2020-09-23 23:58:06 -0700

    gsea converted DEG added with instruction to vizr

--- easyGSEA/server/1.2.server-body1.R
+++ easyGSEA/server/1.2.server-body1.R
@@ -545,7 +545,7 @@ output$ui_deg_download <- renderUI({
                      label = "Download converted DEG table (.csv)", style = rv$dbtn_style,
                      color = rv$dbtn_color, size=rv$dbtn_size, block = F
         ),
-        bsTooltip("deg_download_btn","Download converted DEG table and proceed to easyVizR for multiple comparisons"
+        bsTooltip("deg_download_btn",HTML("Download converted DEG table and proceed to <b>easyVizR</b> for multiple comparisons")
                   ,placement = "top")
     )
     
