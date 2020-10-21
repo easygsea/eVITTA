2d5e18d7ebcbc7afa61a9368d9f7b8adce1ff6bb
yongxingl <yongxingliu0820@gmail.com>
2020-10-21 14:28:40 -0700

    some changes for introjs

--- easyGSEA/server/help.R
+++ easyGSEA/server/help.R
@@ -1,71 +1,106 @@
-output$floating_button_tab1 <- renderUI({
+# the events trigger by pressing the help button on each tabs
+observeEvent(input$help_tab1, {
+      # check the mode of analysis
+      if(input$selected_mode == "gsea"){
+        #first check if the user have upload a file
+          if (is.null(rv$data_head_o)==T){
+          call_introjs(intros$R_pre)
+        } else {
+          # if yes, check if the file has a conversion table
+          if(!is.null(rv$gene_lists_mat1)){
+            # load corresponding introjs for files with converison table
+            call_introjs(intros$R_post_with_conversion_table)
+            }
+          else{
+            call_introjs(intros$R_post)
+            }
+        }
+        # else it must be in ora mode
+      } else {
+        #first check if the user have upload a file
+        if (is.null(rv$gene_lists)==T){
+          call_introjs(intros$R_pre_ora)
+        } else {
+          # if yes, check if the file has a conversion table
+          if(!is.null(rv$gene_lists_mat2)){
+            # load corresponding introjs for files with converison table
+            call_introjs(intros$R_post_with_conversion_table_ora)
+          }
+          else{
+            call_introjs(intros$R_post_ora)
+          }
+        }
+    }
+  
+  
+})
+
+observeEvent(input$help_tab2, {
   if (is.null(rv$data_head_o)==T){
-    div(style="margin-top:10px",
-        actionBttn(
-          inputId = "help_tab1_pre", label=NULL, 
-          icon = icon("question"), style="material-circle", color="primary", size="lg"
-        )
-    )
+    call_introjs(intros$R_pre)
   } else {
+    call_introjs(intros$R_post)
+  }
+})
+
+observeEvent(input$help_tab3, {
+  if (is.null(rv$data_head_o)==T){
+    call_introjs(intros$R_pre)
+  } else {
+    call_introjs(intros$R_post)
+  }
+})
+
+observeEvent(input$help_tab4, {
+  if (is.null(rv$data_head_o)==T){
+    call_introjs(intros$R_pre)
+  } else {
+    call_introjs(intros$R_post)
+  }
+})
+
+# the help buttons fixed on the each tab
+output$floating_button_tab1 <- renderUI({
     div(style="margin-top:10px",
         actionBttn(
-          inputId = "help_tab1_post", label=NULL,
+          inputId = "help_tab1", label=NULL, 
           icon = icon("question"), style="material-circle", color="primary", size="lg"
         )
     )
-  }
 })
 
 output$floating_button_tab2 <- renderUI({
-  if (is.null(rv$data_head_o)==T){
     div(style="margin-top:10px",
         actionBttn(
-          inputId = "help_tab2_pre", label=NULL, 
+          inputId = "help_tab2", label=NULL, 
           icon = icon("question"), style="material-circle", color="primary", size="lg"
         )
     )
-  } else {
-    div(style="margin-top:10px",
-        actionBttn(
-          inputId = "help_tab2_post", label=NULL,
-          icon = icon("question"), style="material-circle", color="primary", size="lg"
-        )
-    )
-  }
+  
 })
 
 output$floating_button_tab3 <- renderUI({
-  if (is.null(rv$data_head_o)==T){
     div(style="margin-top:10px",
         actionBttn(
-          inputId = "help_tab3_pre", label=NULL, 
+          inputId = "help_tab3", label=NULL, 
           icon = icon("question"), style="material-circle", color="primary", size="lg"
         )
     )
-  } else {
-    div(style="margin-top:10px",
-        actionBttn(
-          inputId = "help_tab3_post", label=NULL,
-          icon = icon("question"), style="material-circle", color="primary", size="lg"
-        )
-    )
-  }
+
 })
 
 output$floating_button_tab4 <- renderUI({
-  if (is.null(rv$data_head_o)==T){
     div(style="margin-top:10px",
         actionBttn(
-          inputId = "help_tab4_pre", label=NULL, 
+          inputId = "help_tab4", label=NULL, 
           icon = icon("question"), style="material-circle", color="primary", size="lg"
         )
     )
-  } else {
-    div(style="margin-top:10px",
-        actionBttn(
-          inputId = "help_tab4_post", label=NULL,
-          icon = icon("question"), style="material-circle", color="primary", size="lg"
-        )
-    )
-  }
 })
+
+# the function that call rintrojs
+call_introjs <- function(file_name) {
+  rintrojs::introjs(session, options = list(showStepNumbers=FALSE,
+                                            steps = file_name)
+  )
+}
