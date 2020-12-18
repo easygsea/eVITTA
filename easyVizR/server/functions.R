3dd658dc059a6b01649013a8d806c26d9ca3ba40
Jonas_QD <richmondjonas@gmail.com>
2020-12-17 23:07:31 -0800

    changed for merge

--- easyVizR/server/functions.R
+++ easyVizR/server/functions.R
@@ -288,6 +288,7 @@ remove_ui <- function(id){
 
 # initialize filter presets
 # ------------------------------------------
+p_filter_text="P <:"
 sig_icon <- "star-of-life"
 both_icon <- "sort"
 up_icon <- "arrow-up"
--- easyVizR/server/n_vis_single.R
+++ easyVizR/server/n_vis_single.R
@@ -133,7 +133,7 @@ output$nx_vol_colthresh_opt <- renderUI({
               placement = "top"),
     br(),
     numericInput("nx_p", 
-                 "P <= :", value = rv$nx_p, min = 0, max = 1, step=0.001, width="100px"),
+                 p_filter_text, value = rv$nx_p, min = 0, max = 1, step=0.001, width="100px"),
     numericInput("nx_Stat", 
                  stat_replace1("|Stat| >= :", rv$nx_selected), 
                  value = rv$nx_Stat, min = 0, max = 1, step=0.001, width="100px"),
