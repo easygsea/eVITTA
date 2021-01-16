08a6b0bf0290805d46592cded71b0449d122cc43
Judith Y <47602796+judithayan@users.noreply.github.com>
2021-01-15 18:03:03 -0800

    vizr compartmentalized scatter plots

--- easyVizR/server/functions_vis.R
+++ easyVizR/server/functions_vis.R
@@ -365,7 +365,9 @@ draw_upsetR_with_ins <- function(df, criteria, show_ins=T, color="red",
 
 
 
-
+#======================================================================#
+####                    3.2.1 WORDCLOUD                          ####
+#======================================================================#
 ####--------------------wordcloud for pathways------------------------####
 
 # Set up color palette
@@ -503,5 +505,296 @@ draw_heatmap <- function(df,
     # ,margin = list(l=200)
   )
   
+  fig
+}
+
+
+#======================================================================#
+####                       4.2.1 XY SCATTER                          ####
+#======================================================================#
+
+# plotly two way scatter
+#--------------------------------------------
+
+draw_xy_scatter <- function(to_plot_df,
+                            selected,
+                            plotmode = rv$nxy_sc_plotmode,
+                            sig = rv$nxy_sig,
+                            thresh = rv$nxy_thresh,
+                            nxy_p = rv$nxy_p,
+                            nxy_q = rv$nxy_q,
+                            nxy_stat = rv$nxy_stat,
+                            nxy_size = rv$nxy_sc_size,
+                            logic = rv$n_sc_logic,
+                            outline_width= rv$nxy_sc_outlinewidth,
+                            outline_color = rv$nxy_sc_outlinecolor,
+                            opacity = rv$nxy_sc_opacity,
+                            colormode = rv$nxy_colormode,
+                            df_n=rv$df_n
+){
+  # these are the plotted genes
+  df_ins <- to_plot_df$Name 
+  
+  if (plotmode=="Focus"){ # plot intersection only
+    df <- to_plot_df
+  } else if (plotmode=="Context"){ # plot all genes
+    df <- to_plot_df
+    df <- rbind(df_n[-which(df_n$Name %in% df_ins),], df) # make sure the ins is plotted first
+  }
+  
+  # initialize colnames
+  sig= sig
+  thresh <- thresh
+  xsig <- paste(sig, selected[[1]], sep="_")
+  ysig <- paste(sig, selected[[2]], sep="_")
+  xstat <- paste("Stat", selected[[1]], sep="_")
+  ystat <- paste("Stat", selected[[2]], sep="_")
+  xp <- paste("PValue", selected[[1]], sep="_")
+  yp <- paste("PValue", selected[[2]], sep="_")
+  xq <- paste("FDR", selected[[1]], sep="_")
+  yq <- paste("FDR", selected[[2]], sep="_")
+  print(c(nxy_p, nxy_q, nxy_stat))
+  pthresh <- nxy_p
+  qthresh <- nxy_q
+  statthresh <- nxy_stat
+  
+  req_cols(df, c(xsig, ysig, xstat, ystat, xp, yp, xq, yq))
+  
+  # initialize df
+  df[df==0]<-0.00001 # replace 0 with 0.001
+  df <- remove_nas(df)
+  
+  req(nrow(df)>0)
+  
+  # initialize custom variables
+  discrete_c1 = "red"
+  discrete_c2 = "black"
+  discrete_c3 = "lightgray"
+  size = nxy_size
+  size1 = size+2 # default dot size, initialized to 5
+  size2 = (nxy_size-1) + 3 # size multiplier for the color & size option; initialized to 2+3
+  linewidth = outline_width
+  linecolor=outline_color
+  opacity=opacity # default 0.7
+  
+  
+  # initialize marker settings as none
+  df$color <- discrete_c3
+  df$color <- as.factor(df$color)
+  df$size <- size1
+  
+  if (colormode== "None"){
+    df$color <- as.character(df$color)
+    df$color[which(df$Name %in% df_ins)] <- discrete_c2
+    df$color <- as.factor(df$color)
+    
+    marker_settings <- list(
+      color= df$color, size= df$size, opacity=opacity,
+      line = list(color = linecolor, width = linewidth))
+    
+  } else if (colormode== "Two colors"){ 
+    
+    # color by AND or OR logic
+    df$color <- as.character(df$color)
+    df$color[which(df$Name %in% df_ins)] <- discrete_c2 # dots in the genelist
+    
+    # highlighted dots
+    if (logic == "Both"){
+      df$color[which(
+        df[[xp]] < pthresh & df[[xq]] < qthresh & abs(df[[xstat]]) > statthresh &
+          df[[yp]] < pthresh & df[[yq]] < qthresh & abs(df[[ystat]]) > statthresh
+      )] <- discrete_c1
+    } else if (logic == "Either"){
+      df$color[which(
+        (df[[xp]] < pthresh & df[[xq]] < qthresh & abs(df[[xstat]]) > statthresh) |
+          (df[[yp]] < pthresh & df[[yq]] < qthresh & abs(df[[ystat]]) > statthresh)
+      )] <- discrete_c1
+    }
+    
+    df$color <- as.factor(df$color)
+    df$size <- size1
+    
+    marker_settings <- list(
+      color= df$color, size= df$size, opacity=opacity,
+      line = list(color = linecolor, width = linewidth))
+  }
+  else if (colormode== "Color and size"){
+    
+    df[df==0]<-0.00001 # replace 0 with 0.001
+    df$color <- -log10(as.numeric(df[[xsig]]))
+    df$color <- as.numeric(df$color)
+    df$size <- -log10(as.numeric(df[[ysig]]))* size2
+    #print(head(df))
+    
+    # background dots color
+    df$color[-which(df$Name %in% df_ins)] <- discrete_c3
+    df$size[-which(df$Name %in% df_ins)] <- size1
+    
+    marker_settings <- list(
+      color= df$color, size= df$size,
+      opacity=opacity, line = list(color = linecolor, width = linewidth),
+      colorscale=cscale, cauto=F, cmin=0, cmax=3,
+      colorbar=list(title=paste0('-log10(',sig,'(x))')))
+  }
+  
+  # generate properties table only when two color mode is selected
+  if (colormode!="Color and size"){
+    summary_df <- summarize_factor_column(df, "color")
+    rv$nxy_color <- summary_df
+  }
+  
+  
+  lm_fun <- paste0("`", xstat, "` ~ `", ystat, "`")
+  rv$fit_nxy <- lm(lm_fun, data = df)
+  
+  # print(head(df))
+  
+  stat_replacements <- stat_replace1(rep("Stat",2), selected, mode="each")
+  
+  fig <- plot_ly(
+    data = df, 
+    x = df[[xstat]],
+    y = df[[ystat]],
+    type = 'scatter',
+    mode = 'markers', 
+    marker = marker_settings,
+    hoverinfo="text",
+    text=c(paste0(df$Name, 
+                  "<br>",stat_replacements[[1]],"(x): ", round(df[[xstat]], 3),
+                  "<br>p(x): ", round(df[[xp]], 3),
+                  ", q(x): ", round(df[[xq]], 3),
+                  "<br>",stat_replacements[[2]],"(y): ", round(df[[ystat]], 3),
+                  "<br>p(y): ", round(df[[yp]], 3),
+                  ", q(y): ", round(df[[yq]], 3)
+    ))
+  )
+  fig <- fig %>% layout(title = paste0(selected[[1]], " vs ", selected[[2]], " (n=",nrow(df),")"),
+                        yaxis = list(zeroline = T, title=stat_replace1(paste0("Stat_",selected[[2]]),selected[[2]])),
+                        xaxis = list(zeroline = T, title=stat_replace1(paste0("Stat_",selected[[1]]),selected[[1]]))
+  )
+  
+  return(fig)
+}
+
+#======================================================================#
+####                       4.2.2 3D SCATTER                          ####
+#======================================================================#
+
+
+draw_3d_scatter <- function(to_plot_df,
+                            selected,
+                            plotmode = rv$nxyz_sc_plotmode,
+                            pp = rv$n_3ds_p,
+                            qq = rv$n_3ds_q,
+                            ss = rv$n_3ds_Stat,
+                            size = rv$nxyz_sc_size,
+                            linewidth = rv$nxyz_sc_outlinewidth,
+                            linecolor = rv$nxyz_sc_outlinecolor,
+                            opacity=rv$nxyz_sc_opacity,
+                            colormode = rv$nxyz_colormode,
+                            logic = rv$nxyz_sc_logic,
+                            df_n = rv$df_n
+){
+  
+  # these are the plotted genes
+  df_ins <- to_plot_df$Name 
+  
+  if (plotmode=="Focus"){ # plot intersection only
+    df <- to_plot_df
+  } else if (plotmode=="Context"){ # plot all genes
+    df <- to_plot_df
+    df <- rbind(df_n[-which(df_n$Name %in% df_ins),], df) # make sure the ins is plotted first
+  }
+  
+  df <- remove_nas(df)
+  
+  
+  # get col names
+  statcols <- paste("Stat_", selected, sep="")
+  pcols <- paste("PValue_", selected, sep="")
+  qcols <- paste("FDR_", selected, sep="")
+  pp <- pp
+  qq <- qq
+  ss <- ss
+  
+  discrete_c1a <- "red"
+  discrete_c1b <- "blue"
+  discrete_c2 <- "black"
+  discrete_c3 <- "lightgray"
+  size1 <- size-1 # default size
+  linewidth = linewidth
+  linecolor=linecolor
+  opacity=opacity # default 0.7
+  
+  
+  incProgress(0.2)
+  
+  # default color is here. conditionally color by threshold.
+  df$color <- discrete_c3
+  df$color[which(df$Name %in% df_ins)] <- discrete_c2
+  # assign color by certain criteria
+  if (colormode == "Two colors"){
+    if (logic == "Both"){
+      req(is.null(logic)==F)
+      df$color <- ifelse(
+        (df[[pcols[[1]]]] < pp & df[[qcols[[1]]]] < qq & df[[statcols[[1]]]] > ss) &
+          (df[[pcols[[2]]]] < pp & df[[qcols[[2]]]] < qq & df[[statcols[[2]]]] > ss) &
+          (df[[pcols[[3]]]] < pp & df[[qcols[[3]]]] < qq & df[[statcols[[3]]]] > ss), 
+        discrete_c1a, df$color)
+      df$color <- ifelse(
+        (df[[pcols[[1]]]] < pp & df[[qcols[[1]]]] < qq & df[[statcols[[1]]]] < -ss) &
+          (df[[pcols[[2]]]] < pp & df[[qcols[[2]]]] < qq & df[[statcols[[2]]]] < -ss) &
+          (df[[pcols[[3]]]] < pp & df[[qcols[[3]]]] < qq & df[[statcols[[3]]]] < -ss), 
+        discrete_c1b, df$color)
+    } else if (logic == "Either"){
+      df$color <- ifelse(
+        (df[[pcols[[1]]]] < pp & df[[qcols[[1]]]] < qq & df[[statcols[[1]]]] > ss) |
+          (df[[pcols[[2]]]] < pp & df[[qcols[[2]]]] < qq & df[[statcols[[2]]]] > ss) |
+          (df[[pcols[[3]]]] < pp & df[[qcols[[3]]]] < qq & df[[statcols[[3]]]] > ss), 
+        discrete_c1a, df$color)
+      df$color <- ifelse(
+        (df[[pcols[[1]]]] < pp & df[[qcols[[1]]]] < qq & df[[statcols[[1]]]] < -ss) |
+          (df[[pcols[[2]]]] < pp & df[[qcols[[2]]]] < qq & df[[statcols[[2]]]] < -ss) |
+          (df[[pcols[[3]]]] < pp & df[[qcols[[3]]]] < qq & df[[statcols[[3]]]] < -ss), 
+        discrete_c1b, df$color)
+    }
+    df$color <- as.factor(df$color)
+  }
+  
+  incProgress(0.2)
+  
+  stat_replacements <- stat_replace1(rep("Stat",3), selected, mode="each")
+  
+  # datapoint appearance
+  marker_settings <- list(
+    color = df$color, size=size1,
+    opacity=opacity, line = list(color = linecolor, width = linewidth)
+  )
+  
+  fig <- plot_ly(df, x = df[[statcols[[1]]]], y = df[[statcols[[2]]]], z = df[[statcols[[3]]]], marker = marker_settings,
+                 hoverinfo="text",
+                 text=c(paste0(
+                   df$Name, 
+                   "<br>",stat_replacements[[1]],"(x):", round(df[[statcols[[1]]]], 3),
+                   "<br>p=", round(df[[pcols[[1]]]], 3),", q=", round(df[[qcols[[1]]]], 3),
+                   "<br>",stat_replacements[[2]],"(y):", round(df[[statcols[[2]]]], 3),
+                   "<br>p=", round(df[[pcols[[2]]]], 3),", q=", round(df[[qcols[[2]]]], 3),
+                   "<br>",stat_replacements[[3]],"(z):", round(df[[statcols[[3]]]], 3),
+                   "<br>p=", round(df[[pcols[[3]]]], 3),", q=", round(df[[qcols[[3]]]], 3)
+                 )))
+  
+  incProgress(0.2)
+  
+  # generate properties table
+  summary_df <- summarize_factor_column(df, "color")
+  rv$n_3ds_prop <- summary_df
+  
+  fig <- fig %>% add_markers()
+  fig <- fig %>% layout(title = paste0('3D Scatter, n=',nrow(df)),
+                        scene = list(xaxis = list(title = paste0(statcols[[1]])),
+                                     yaxis = list(title = paste0(statcols[[2]])),
+                                     zaxis = list(title = paste0(statcols[[3]]))))
+  
+  # })
   fig
 }
