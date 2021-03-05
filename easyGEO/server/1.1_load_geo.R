6da480960429df23c3aba7a682d7f396cfa4c39d
yongxingl <yongxingliu0820@gmail.com>
2021-03-05 10:41:00 -0800

    easyVizR help page updated

--- easyGEO/server/1.1_load_geo.R
+++ easyGEO/server/1.1_load_geo.R
@@ -2,6 +2,9 @@
 # library(later)
 # # the modal to remind the user it is a demo session
 # observe({
+#   # updateRadioButtons(session, inputId = "selected_mode", selected = "manual")
+#   # To activate the manual uploads demo session, change the default value of rv$run_mode
+#   # and the default value of input$selected_mode to "manual".
 #   init_demo()
 #   showModal(modalDialog(title = tags$h3("Welcome to our easyGEO demo session"),
 #                         tags$h4("Explore the sample output that performs interactively in the same way as real output.")
@@ -16,15 +19,26 @@
 # observeEvent(input$welcome_modal, {
 #   removeModal()
 #   rv$demo_yes <- "yes"
-#   call_introjs(rbind(intros$E_pre,intros$E_post,intros$E_post_with_summary_ui))
+#   if(rv$run_mode == "auto")
+#     call_introjs(rbind(intros$E_pre,intros$E_post,intros$E_post_with_summary_ui))
+#   else
+#     call_introjs(intros$E_manual)
 #   print(input$menu1)
 # 
 # })
 # 
 # # start rintrojs when users switch tabs
 # observeEvent(input$menu1,{
-#   if(input$menu1 == "tab3"){
-#     later::later(~call_introjs(intros$D_post), delay = 2)
+#   if(input$menu1 == "tab1" && !is.null(rv$demo_yes)){
+#     if(rv$run_mode == "auto")
+#       later::later(~call_introjs(rbind(intros$E_pre,intros$E_post,intros$E_post_with_summary_ui)), delay = 2)
+#     else
+#       later::later(~call_introjs(intros$E_manual), delay = 2)
+#   } else if(input$menu1 == "tab3"){
+#     if(rv$run_mode == "auto")
+#       later::later(~call_introjs(intros$D_post), delay = 2)
+#     else
+#       later::later(~call_introjs(intros$D_manual), delay = 2)
 #   } else if(input$menu1 == "tab2"){
 #     later(~call_introjs(intros$F_post),2)
 #   } else if(input$menu1 == "tab4"){
